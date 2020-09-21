d88b87da1dfbe34117a7e496936d92dad267fe17
twMisc <ccchang.am09g@nctu.edu.tw>
2020-09-21 14:41:55 +0800

    upload example model

---
+++ draw_figure.py
@@ -0,0 +1,9 @@
+#%% load needed packages
+import tensorflow as tf
+import matplotlib.pyplot as plt 
+import numpy as np
+# %%
+model= tf.keras.models.load_model('model_US_239')
+# %%
+print('hello')
+# %%
